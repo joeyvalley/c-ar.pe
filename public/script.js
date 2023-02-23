dc2228f9ad5d7867d36568a97e9c9d4d986c9c91
Joseph Valle <josephrvalle@gmail.com>
2023-02-23 11:33:21 -0500

    edited the frontend JS script, added a model for a user

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-// Global variables.
+// Create our date variables for use throughout the script.
 const today = new Date();
 const day = String(today.getDate());
 const month = String(today.getMonth() + 1).padStart(2, '0');
@@ -6,8 +6,29 @@ const monthArr = ["January", "February", "March", "April", "May", "June", "July"
 const monthStr = monthArr[+month - 1];
 const year = today.getFullYear();
 
+
+// Select elements from the DOM for manipulation.
 const form = document.getElementById("calendarForm");
 const calendar = document.getElementById("calendar");
+const btn = document.getElementById("submitBtn");
+const login = document.getElementById("login");
+
+// Set the values for the calendar input.
+const inputVal = year + "-" + month + "-" + day;
+const minVal = +(year - 1) + "-" + month + "-" + day;
+calendar.value = inputVal;
+calendar.max = inputVal;
+calendar.min = minVal;
+
+// Add event listeners to the submit button to change the cursor style when hovering.
+btn?.addEventListener('mouseover', () => {
+  btn.classList.add("hover");
+})
+btn?.addEventListener('mouseout', () => {
+  btn.classList.remove("hover");
+})
+
+// Add event listener to the form in order to make API calls.
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
   const month = calendar.value.split("-")[1];
@@ -18,3 +39,10 @@ form.addEventListener('submit', async (event) => {
   console.log(json);
 })
 
+// Add event listeners to the login to make it appear as a link, won't do anything for now.
+login?.addEventListener('mouseover', () => {
+  login.classList.add("fake-link");
+})
+login?.addEventListener('mouseout', () => {
+  login.classList.remove("fake-link");
+})
